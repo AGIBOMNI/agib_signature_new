64e33f33abc95d0a298d1e79c9ffbd17596c9cb4
AGIBOMNI <agib.gambia@gmail.com>
2025-09-30 09:57:03 +0000

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'AGIB Email Signature Generator',
+  description: 'Created with AGIB Email Signature Generator',
+  generator: 'AGIB Email Signature Generator',
 }
 
 export default function RootLayout({
